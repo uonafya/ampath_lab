e2ad1d8f68759c83b1487933aa8f135c46aaeebb
Your Name <joelkith@gmail.com>
2021-03-12 15:58:20 +0300

    Updates

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,18 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Load the following SQL files
+        // complete.sql
+        // facilitys.sql
+
         // dd('What the hell do you think you are doing!! We see you!!!');
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(FakerSeeder::class);        
-        // $this->call(DrSeeder::class);
-        // $this->call(KitsSeeder::class);    
-        // $this->call(LabEquipmentMailingSeeder::class);
-        // $this->call(GeneralConsumablesSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(KitsSeeder::class);    
+        $this->call(LabEquipmentMailingSeeder::class);
+        $this->call(GeneralConsumablesSeeder::class);
         $this->call(CovidLookupSeeder::class);
-        // $this->call(CovidKitsSeeder::class);
-        // $this->call(TestTypeSeeder::class);
-        // $this->call(TestTypeSeeder::class);
-        // $this->call(ManualMachineSeeder::class);
-        // $this->call(CancerLookupsSeeder::class);
+        $this->call(CovidKitsSeeder::class);
+        $this->call(TestTypeSeeder::class);
+        $this->call(ManualMachineSeeder::class);
+        $this->call(CancerLookupsSeeder::class);
     }
 }
