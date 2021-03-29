d8d4e206d0addd7e0885ee72acdac16ca076a867
Joshua Bakasa <baksajoshua09@gmail.com>
2021-03-29 10:48:41 +0000

    After disk corruption

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // dd('What the hell do you think you are doing!! We see you!!!');
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(FakerSeeder::class);        
+        // $this->call(DrSeeder::class);
+        // $this->call(KitsSeeder::class);    
+        // $this->call(LabEquipmentMailingSeeder::class);
+        // $this->call(GeneralConsumablesSeeder::class);
+        $this->call(CovidLookupSeeder::class);
+        // $this->call(CovidKitsSeeder::class);
+        // $this->call(TestTypeSeeder::class);
+        // $this->call(TestTypeSeeder::class);
+        // $this->call(ManualMachineSeeder::class);
+        // $this->call(CancerLookupsSeeder::class);
+    }
+}
