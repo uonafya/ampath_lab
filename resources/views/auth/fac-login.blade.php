1b95e748799a56f7567dd54ab9b5536a5b043c06
Your Name <joelkith@gmail.com>
2021-05-11 09:06:45 +0300

    Updates

--- resources/views/auth/fac-login.blade.php
+++ resources/views/auth/fac-login.blade.php
@@ -18,6 +18,11 @@
         <div class="col-md-12">
             <div class="hpanel" style="width: 430px;">
                 <div class="panel-body" style="padding: 20px;">
+                    <div class="alert alert-info">
+                        <center>
+                            You can now login with a batch that is up to one year old.
+                        </center>                        
+                    </div>
                     <form class="form-horizontal" method="POST" action="{{ url('login/facility') }}">
                         @csrf
                         <div class="form-group" style="padding-bottom: -;padding-right: 20px;padding-left: 20px;margin-bottom: 16px;margin-top: 10px;">
