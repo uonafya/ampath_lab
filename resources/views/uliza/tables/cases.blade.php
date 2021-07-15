af5f7ced895158364f4bb93fe87c263d5569eab7
Joel <joelkith@gmail.com>
2021-07-15 12:26:20 +0300

    Updates

--- resources/views/uliza/tables/cases.blade.php
+++ resources/views/uliza/tables/cases.blade.php
@@ -7,106 +7,108 @@
     <form action="{{ url('/uliza-form/index') }}" method="POST">
         @csrf
 
-        <div class="col-md-4"> 
-            <div class="form-group">
-                <label class="col-sm-3 control-label">Select Status</label>
-                <div class="col-sm-9">
-                    <select class="form-control" name="status_id">
-                        <option></option>
-                        @foreach ($statuses as $status)
-                            <option value="{{ $status->id }}"
+        <div class="row">
+            <div class="col-md-4"> 
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">Select Status</label>
+                    <div class="col-sm-9">
+                        <select class="form-control" name="status_id">
+                            <option></option>
+                            @foreach ($statuses as $status)
+                                <option value="{{ $status->id }}"
 
-                            @if (isset($status_id) && $status_id == $status->id)
-                                selected
-                            @endif
+                                @if (isset($status_id) && $status_id == $status->id)
+                                    selected
+                                @endif
 
-                            > {{ $status->name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>                        
+                                > {{ $status->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>                        
+                </div> 
+            </div>
+            <div class="col-md-4"> 
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">Select Subcounty</label>
+                    <div class="col-sm-9">
+                        <select class="form-control" name="subcounty_id" id="subcounty_id">
+                            <option></option>
+                            @foreach ($subcounties as $subcounty)
+                                <option value="{{ $subcounty->id }}"
+
+                                @if (isset($subcounty_id) && $subcounty_id == $subcounty->id)
+                                    selected
+                                @endif
+
+                                > {{ $subcounty->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>                        
+                </div> 
+            </div>
+            <div class="col-md-4"> 
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">Select County</label>
+                    <div class="col-sm-9">
+                        <select class="form-control" name="county_id" id="county_id">
+                            <option></option>
+                            @foreach ($counties as $county)
+                                <option value="{{ $county->id }}"
+
+                                @if (isset($county_id) && $county_id == $county->id)
+                                    selected
+                                @endif
+
+                                > {{ $county->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>                        
+                </div> 
+            </div>
+            <div class="col-md-4"> 
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">Select Twg</label>
+                    <div class="col-sm-9">
+                        <select class="form-control" name="twg_id" id="twg_id">
+                            <option></option>
+                            @foreach ($twgs as $twg)
+                                <option value="{{ $twg->id }}"
+
+                                @if (isset($twg_id) && $twg_id == $twg->id)
+                                    selected
+                                @endif
+
+                                > {{ $twg->twg }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>                        
+                </div> 
+            </div>
+            <div class="col-md-4"> 
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">Start Date</label>
+                    <div class="col-sm-9">
+                        <input type="text" name="start_date" class="form-control date" />
+                    </div>                        
+                </div> 
+            </div>
+            <div class="col-md-4"> 
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">End Date</label>
+                    <div class="col-sm-9">
+                        <input type="text" name="end_date" class="form-control date" />
+                    </div>                        
+                </div> 
+            </div>
+
+            <div class="col-sm-2">                
+                <button class="btn btn-primary" id="date_range">Filter</button>  
             </div> 
-        </div>
-        <div class="col-md-4"> 
-            <div class="form-group">
-                <label class="col-sm-3 control-label">Select Subcounty</label>
-                <div class="col-sm-9">
-                    <select class="form-control" name="subcounty_id" id="subcounty_id">
-                        <option></option>
-                        @foreach ($subcounties as $subcounty)
-                            <option value="{{ $subcounty->id }}"
-
-                            @if (isset($subcounty_id) && $subcounty_id == $subcounty->id)
-                                selected
-                            @endif
-
-                            > {{ $subcounty->name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>                        
-            </div> 
-        </div>
-        <div class="col-md-4"> 
-            <div class="form-group">
-                <label class="col-sm-3 control-label">Select County</label>
-                <div class="col-sm-9">
-                    <select class="form-control" name="county_id" id="county_id">
-                        <option></option>
-                        @foreach ($counties as $county)
-                            <option value="{{ $county->id }}"
-
-                            @if (isset($county_id) && $county_id == $county->id)
-                                selected
-                            @endif
-
-                            > {{ $county->name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>                        
-            </div> 
-        </div>
-        <div class="col-md-4"> 
-            <div class="form-group">
-                <label class="col-sm-3 control-label">Select Twg</label>
-                <div class="col-sm-9">
-                    <select class="form-control" name="twg_id" id="twg_id">
-                        <option></option>
-                        @foreach ($twgs as $twg)
-                            <option value="{{ $twg->id }}"
-
-                            @if (isset($twg_id) && $twg_id == $twg->id)
-                                selected
-                            @endif
-
-                            > {{ $twg->twg }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>                        
-            </div> 
-        </div>
-        <div class="col-md-4"> 
-            <div class="form-group">
-                <label class="col-sm-3 control-label">Start Date</label>
-                <div class="col-sm-9">
-                    <input type="text" name="start_date" class="form-control date" />
-                </div>                        
-            </div> 
-        </div>
-        <div class="col-md-4"> 
-            <div class="form-group">
-                <label class="col-sm-3 control-label">End Date</label>
-                <div class="col-sm-9">
-                    <input type="text" name="end_date" class="form-control date" />
-                </div>                        
-            </div> 
-        </div>
-
-        <div class="col-sm-2">                
-            <button class="btn btn-primary" id="date_range">Filter</button>  
-        </div>  
+        </div> 
 
         
     </form>
