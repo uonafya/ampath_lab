9e7b572545334e7e28de2b0d1afffc9e162c81ec
Joshua Bakaa <baksajoshua09@gmail.com>
2021-08-11 13:41:29 +0300

    Fixing the EID Problem

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -43,10 +43,9 @@ class SampleController extends Controller
             ->whereRaw($string)
             ->where(['site_entry' => 2])
             ->orderBy('id', 'desc')
-            ->get();
-            // ->paginate(50);
+            ->paginate(50);
         
-        // $samples->setPath(url()->current());
+        $samples->setPath(url()->current());
         $data['samples'] = $samples;
         $data['pre'] = '';
         return view('tables.poc_samples', $data)->with('pageTitle', 'Eid POC Samples');
